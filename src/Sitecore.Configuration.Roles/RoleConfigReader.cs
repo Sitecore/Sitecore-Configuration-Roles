b463801dfd5ade9dc4a1686683b0135d32505fd3
AlenPelin <AlenPelin@gmail.com>
2017-06-26 17:00:35 +1000

    Implement include:override setting (closes #13)

--- src/Sitecore.Configuration.Roles/RoleConfigReader.cs
+++ src/Sitecore.Configuration.Roles/RoleConfigReader.cs
@@ -1,6 +1,8 @@
 ﻿namespace Sitecore.Configuration.Roles
 {
+  using System;
   using System.Collections;
+  using System.IO;
   using System.Xml;
   using Sitecore.Diagnostics;
   using Sitecore.Xml.Patch;
@@ -8,10 +10,13 @@
   [UsedImplicitly]
   public class RoleConfigReader : ConfigReader
   {
+    [NotNull]
+    private readonly string[] IncludeOverride = ReadIncludeOverride();
+
     private readonly RoleConfigurationHelper RoleConfigurationHelper;
 
     private readonly XmlPatcher Patcher;
-
+    
     public RoleConfigReader()
     {
       RoleConfigurationHelper = new RoleConfigurationHelper();
@@ -43,6 +48,41 @@
       Assert.ArgumentNotNull(element, "element");
 
       return new ConfigPatcher(element, this.Patcher);
+    }
+
+    protected override void LoadAutoIncludeFiles(XmlNode element)
+    {
+      Assert.ArgumentNotNull(element, "element");
+
+      if (IncludeOverride.Length == 0)
+      {
+        base.LoadAutoIncludeFiles(element);
+
+        return;
+      }
+      
+      var configPatcher = GetConfigPatcher(element);
+      LoadAutoIncludeFiles(configPatcher, MainUtil.MapPath("/App_Config/Sitecore/Components"));
+
+      foreach (var path in IncludeOverride)
+      {
+        Assert.IsTrue(Directory.Exists(path), "The include:override setting points to non-existing folder: {0}", path);
+        Assert.IsTrue(Path.IsPathRooted(path), "The include:override setting points to non-rooted path: {0}", path);
+
+        LoadAutoIncludeFiles(configPatcher, path);
+      }
+    }
+
+    [NotNull]
+    private static string[] ReadIncludeOverride()
+    {
+      var includeOverride = System.Configuration.ConfigurationManager.AppSettings["include:override"];
+      if (string.IsNullOrEmpty(includeOverride))
+      {
+        return new string[0];
+      }
+
+      return includeOverride.Split(";|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
     }
   }
 }
